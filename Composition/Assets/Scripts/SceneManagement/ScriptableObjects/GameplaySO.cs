92c345b6a739b17507445440032c0cd0fae23286
jstPlink <40009884+jstPlink@users.noreply.github.com>
2023-08-08 22:49:37 +0200

    removed projects

--- Composition/Assets/Scripts/SceneManagement/ScriptableObjects/GameplaySO.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-/// <summary>
-/// This class contains Settings specific to Gameplay scenes only
-/// </summary>
-
-[CreateAssetMenu(fileName = "NewGameplayScene", menuName = "Scene Data/Gameplay")]
-
-public class GameplaySO : GameSceneSO { }
--- Composition/Assets/Scripts/SceneManagement/ScriptableObjects/PersistentManagersSO.cs
+++
@@ -1,5 +0,0 @@
-﻿using UnityEngine;
-
-//CreateAssetMenu commented since we don't want to create more than one Initialisation GameSceneSO
-//[CreateAssetMenu(fileName = "PersistentManagers", menuName = "Scene Data/PersistentManagers")]
-public class PersistentManagersSO : GameSceneSO { }
